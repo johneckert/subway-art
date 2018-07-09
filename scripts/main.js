d424e2f0476b80ffea62a1949f8af82df31e2b5b
John Eckert <johnteckert@gmail.com>
2018-07-09 12:40:37 -0400

    successfully retrieving location data

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,21 @@
-fetch("http://localhost:3000/")
-  .then(res => res.json())
-  .then(json => console.log(json));
+$(document).ready(function() {
+  getUserLocation();
+});
+
+function getUserLocation() {
+  if ("geolocation" in navigator) {
+    // geolocation is available
+    navigator.geolocation.getCurrentPosition(function(position) {
+      console.log(position.coords.latitude, position.coords.longitude);
+    });
+  } else {
+    // geolocation IS NOT available
+    console.log("no position available");
+  }
+}
+
+function getSubwayInfo(locationData) {
+  fetch("http://localhost:3000/", {})
+    .then(res => res.json())
+    .then(json => console.log(json));
+}
