ee221006c59b87f61119d8c020ad5b9f38c23100
John Eckert <johnteckert@gmail.com>
2018-07-09 14:34:27 -0400

    added fetch for subway info

--- scripts/main.js
+++ scripts/main.js
@@ -3,19 +3,35 @@ $(document).ready(function() {
 });
 
 function getUserLocation() {
+  //set default location - Carnegie Hall
+  let latitude = 40.765061;
+  let longitude = -73.979942;
   if ("geolocation" in navigator) {
-    // geolocation is available
+    // geolocation IS available
     navigator.geolocation.getCurrentPosition(function(position) {
-      console.log(position.coords.latitude, position.coords.longitude);
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
+      //will need to send location to backend inside here
+      console.log("yes", latitude, longitude);
     });
   } else {
     // geolocation IS NOT available
     console.log("no position available");
+    //stump Carnegie Hall
   }
+  console.log("end", latitude, longitude);
+  getSubwayInfo(latitude, longitude);
 }
 
-function getSubwayInfo(locationData) {
-  fetch("http://localhost:3000/", {})
+function getSubwayInfo(latitude, longitude) {
+  fetch("http://localhost:3000/", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ lat: latitude, lon: longitude })
+  })
     .then(res => res.json())
     .then(json => console.log(json));
 }
